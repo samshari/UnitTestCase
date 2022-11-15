b0fadde01817bc465f9ff12ea19c8977cb247d6c
roopam <roopam.singh@qualitestgroup.com>
2022-11-15 14:24:35 +0530

    PerformProgress Model

---
+++ Exelon.Domain/Entities/PerformProgressModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exelon.Domain.Entities
+{
+   public class PerformProgressModel
+    {
+        public Int64 PerformProgressId { get; set; }
+        public int PDId { get; set; }
+        public Int64 LinkingId { get; set; }
+        public DateTime WeeklyDate { get; set; }
+        public decimal Miles { get; set; }
+        public string PDName { get; set; }
+        public string WorkOrder { get; set; }
+        public string ProjectID { get; set; }
+        public string ProjectManager { get; set; }
+        public decimal OHMiles { get; set; }
+        public decimal UGMiles { get; set; }
+    }
+}
